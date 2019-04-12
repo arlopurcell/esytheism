8195ed89ddd968efa85b17d6d71017e1888093dc
arlo <arlo@indeed.com>
2019-04-12 10:16:21 -0500

    make both clouds and temps viewable

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,15 @@ extern crate quicksilver;
 extern crate rand;
 extern crate rayon;
 
-use rand::distributions::{Distribution, Uniform};
+use rand::distributions::{Distribution, Uniform, Normal};
 use rayon::prelude::*;
 
 use quicksilver::{
     Result,
     geom::{Rectangle, Vector},
     graphics::{Background::Col, Color},
-    lifecycle::{Settings, State, Window, run},
+    input::{ButtonState, Key},
+    lifecycle::{Event, Settings, State, Window, run},
 };
 
 const WORLD_SIZE: usize = 100;
@@ -17,11 +18,30 @@ const TICKS_PER_DAY: i32 = 10;
 const DAYS_PER_YEAR: i32 = 100;
 const TICKS_PER_YEAR: i32 = TICKS_PER_DAY * DAYS_PER_YEAR;
 const MAX_TEMP: i32 = 100;
+const MAX_CLOUD: i32 = 100;
 
 struct GameState {
+    world: World,
+    view: ViewType,
+}
+
+enum ViewType {
+    Temp,
+    Cloud,
+}
+
+struct World {
     counter: i32,
+
     temps: WorldComponent<Vec<i32>>,
     target_temps: WorldComponent<Vec<i32>>,
+
+    cloud_threshold: i32,
+    rain_threshold: i32,
+    clouds: WorldComponent<Vec<i32>>,
+    target_clouds: WorldComponent<Vec<i32>>,
+    // seasonal_cloud_phases: Vec<i32>,
+    // seasonal_cloud_amplitude: Vec<i32>,
 }
 
 struct WorldComponent<T> {
@@ -44,13 +64,19 @@ impl<T> WorldComponent<T> {
     }
 }
 
-impl GameState {
+impl World {
     fn tick(&mut self) {
         {
             let (temps, next_temps) = self.temps.phases();
             let (target_temps, next_target_temps) = self.target_temps.phases();
+            let (clouds, next_clouds) = self.clouds.phases();
+            let (target_clouds, _next_target_clouds) = self.target_clouds.phases();
+
+            let cloud_threshold = &self.cloud_threshold;
+            let rain_threshold = &self.rain_threshold;
 
             if self.counter % TICKS_PER_DAY == 0 {
+                // Update target temps
                 let equator = ((((TICKS_PER_YEAR / 2) - self.counter % TICKS_PER_YEAR).abs() - (TICKS_PER_YEAR / 4)) * (WORLD_SIZE as i32 / 3) / TICKS_PER_YEAR) + (WORLD_SIZE as i32 / 2);
                 next_target_temps.par_iter_mut().enumerate().for_each(|(n, next_target_temp)| {
                     let y = n / WORLD_SIZE;
@@ -58,7 +84,7 @@ impl GameState {
                     *next_target_temp = WORLD_SIZE as i32 - 2 * distance_from_equator;
                 });
 
-
+                // Update temps
                 let temp_sampler = Uniform::from(-10..10);
                 next_temps.par_iter_mut().enumerate().for_each(|(n, next_temp)| {
                     let mut rng = rand::thread_rng();
@@ -106,6 +132,64 @@ impl GameState {
                     *next_temp += target_temps[i * WORLD_SIZE + j];
                     *next_temp /= divisor + 1;
                     *next_temp += temp_sampler.sample(&mut rng);
+
+                    if clouds[n] > *cloud_threshold {
+                        // up to 10 degrees cooler in the shade
+                        *next_temp -= (clouds[n].min(*rain_threshold) - *cloud_threshold) / 2;
+                    }
+                });
+                
+                // Update clouds
+                let cloud_sampler = Normal::new(0.0, 15.0);
+                next_clouds.par_iter_mut().enumerate().for_each(|(n, next_cloud)| {
+                    let mut rng = rand::thread_rng();
+                    let y = n / WORLD_SIZE;
+                    let x = n % WORLD_SIZE;
+
+                    let mut divisor = 1;
+                    *next_cloud = 0;
+                    *next_cloud += clouds[y * WORLD_SIZE + x];
+
+                    if y != (WORLD_SIZE - 1) {
+                        *next_cloud += clouds[(y + 1) * WORLD_SIZE + x];
+                        divisor += 1;
+                    }
+                    if y != 0 {
+                        *next_cloud += clouds[(y - 1) * WORLD_SIZE + x];
+                        divisor += 1;
+                    }
+
+                    if x != (WORLD_SIZE - 1) {
+                        *next_cloud += clouds[y * WORLD_SIZE + x + 1];
+                        divisor += 1;
+                        if y != (WORLD_SIZE - 1) {
+                            *next_cloud += clouds[(y + 1) * WORLD_SIZE + x + 1];
+                            divisor += 1;
+                        }
+                        if y != 0 {
+                            *next_cloud += clouds[(y - 1) * WORLD_SIZE + x + 1];
+                            divisor += 1;
+                        }
+                    }
+
+                    if x != 0 {
+                        *next_cloud += clouds[y * WORLD_SIZE + x - 1];
+                        divisor += 1;
+                        if y != (WORLD_SIZE - 1) {
+                            *next_cloud += clouds[(y + 1) * WORLD_SIZE + x - 1];
+                            divisor += 1;
+                        }
+                        if y != 0 {
+                            *next_cloud += clouds[(y - 1) * WORLD_SIZE + x - 1];
+                            divisor += 1;
+                        }
+                    }
+                    *next_cloud *= 5;
+                    divisor *= 5;
+
+                    *next_cloud += target_clouds[y * WORLD_SIZE + x];
+                    *next_cloud /= divisor + 1;
+                    *next_cloud += cloud_sampler.sample(&mut rng) as i32 * 2;
                 });
             }
         }
@@ -113,6 +197,9 @@ impl GameState {
         if self.counter % TICKS_PER_DAY == 0 {
             self.temps.swap();
             self.target_temps.swap();
+
+            self.clouds.swap();
+            self.target_clouds.swap();
         }
 
         self.counter += 1;
@@ -129,39 +216,91 @@ impl State for GameState {
             }
         }
         Ok(GameState {
-            temps: WorldComponent {
-                phase_t: temps.clone(),
-                phase_f: temps.clone(),
-                phase: true,
+            view: ViewType::Temp,
+            world: World {
+                counter: 0,
+
+                temps: WorldComponent {
+                    phase_t: temps.clone(),
+                    phase_f: temps.clone(),
+                    phase: true,
+                },
+                target_temps: WorldComponent {
+                    phase_t: temps.clone(),
+                    phase_f: temps,
+                    phase: true,
+                },
+
+                cloud_threshold: 50,
+                rain_threshold: 80,
+                clouds: WorldComponent {
+                    phase_t: vec![20; WORLD_SIZE * WORLD_SIZE],
+                    phase_f: vec![20; WORLD_SIZE * WORLD_SIZE],
+                    phase: true,
+                },
+                target_clouds: WorldComponent {
+                    phase_t: vec![20; WORLD_SIZE * WORLD_SIZE],
+                    phase_f: vec![20; WORLD_SIZE * WORLD_SIZE],
+                    phase: true,
+                },
             },
-            target_temps: WorldComponent {
-                phase_t: temps.clone(),
-                phase_f: temps,
-                phase: true,
-            },
-            counter: 0,
         })
     }
 
+    fn event(&mut self, event: &Event, window: &mut Window) -> Result<()> {
+            match *event {
+                Event::Key(Key::T, ButtonState::Pressed) => {
+                    self.view = ViewType::Temp;
+                }
+                Event::Key(Key::C, ButtonState::Pressed) => {
+                    self.view = ViewType::Cloud;
+                }
+                Event::Key(Key::Escape, ButtonState::Pressed) => {
+                    window.close();
+                }
+                _ => (),
+            }
+            Ok(())
+    }
+
     fn update(&mut self, _window: &mut Window) -> Result<()> {
-        self.tick();
+        self.world.tick();
         Ok(())
     }
 
     fn draw(&mut self, window: &mut Window) -> Result<()> {
-        window.clear(Color::BLACK)?;
+        match self.view {
+            ViewType::Temp => {
+                window.clear(Color::BLACK)?;
 
-        let (temps, _) = self.temps.phases();
-        for x in 0..WORLD_SIZE {
-            for y in 0..WORLD_SIZE {
-                window.draw(
-                    &Rectangle::new((x as i32 * 6 + WORLD_SIZE as i32, y as i32 * 6), (6, 6)), 
-                    Col(heatmap_value(temps[y * WORLD_SIZE + x], 0, MAX_TEMP, vec![Color::BLUE, Color::CYAN, Color::GREEN, Color::YELLOW, Color::RED])),
-                );
-            }
+                let (temps, _) = self.world.temps.phases();
+                for x in 0..WORLD_SIZE {
+                    for y in 0..WORLD_SIZE {
+                        window.draw(
+                            &Rectangle::new((x as i32 * 6 + WORLD_SIZE as i32, y as i32 * 6), (6, 6)), 
+                            Col(heatmap_value(temps[y * WORLD_SIZE + x], 0, MAX_TEMP, vec![Color::BLUE, Color::CYAN, Color::GREEN, Color::YELLOW, Color::RED])),
+                        );
+                    }
+                }
+
+                Ok(())
+            },
+            ViewType::Cloud => {
+                window.clear(Color::BLACK)?;
+
+                let (clouds, _) = self.world.clouds.phases();
+                for x in 0..WORLD_SIZE {
+                    for y in 0..WORLD_SIZE {
+                        window.draw(
+                            &Rectangle::new((x as i32 * 6 + WORLD_SIZE as i32, y as i32 * 6), (6, 6)), 
+                            Col(heatmap_value(clouds[y * WORLD_SIZE + x], 0, MAX_TEMP, vec![Color::BLACK, Color::BLACK, Color::WHITE, Color::BLUE])),
+                        );
+                    }
+                }
+
+                Ok(())
+            },
         }
-
-        Ok(())
     }
 }
 
