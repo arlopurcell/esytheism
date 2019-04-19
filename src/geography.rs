2a1f1c559c0087948984e9181aef9b5a7eabd62d
arlo <arlo@indeed.com>
2019-04-19 09:30:36 -0500

    Removing weather stuff. adding pathfinding

---
+++ src/geography.rs
@@ -0,0 +1,136 @@
+use std::cmp::Ordering;
+use std::collections::{HashMap, HashSet, BinaryHeap};
+use std::u32;
+use num_traits::ops::saturating::Saturating;
+
+pub struct Geography {
+    tile_costs: Vec<Vec<u32>>,
+    width: usize,
+    height: usize,
+}
+
+#[derive(Copy, Clone, Eq, PartialEq, Hash)]
+pub struct TilePoint {
+    pub x: usize,
+    pub y: usize,
+}
+
+#[derive(Copy, Clone, Eq, PartialEq)]
+struct PathState {
+    cost: u32,
+    position: TilePoint,
+}
+
+impl Ord for PathState {
+    fn cmp(&self, other: &PathState) -> Ordering {
+        other.cost.cmp(&self.cost)
+            .then_with(|| self.position.x.cmp(&other.position.x))
+            .then_with(|| self.position.y.cmp(&other.position.y))
+    }
+}
+
+impl PartialOrd for PathState {
+    fn partial_cmp(&self, other: &PathState) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Geography {
+
+    pub fn new(tile_costs: Vec<Vec<u32>>, width: usize, height: usize) -> Geography {
+        Geography {
+            tile_costs: tile_costs,
+            height: height,
+            width: width,
+        }
+    }
+
+    pub fn find_path(&self, start: TilePoint, goal: TilePoint) -> Option<Vec<TilePoint>> {
+        let mut closedSet = HashSet::new();
+
+        let mut cameFrom = HashMap::new();
+
+        let mut gScore = HashMap::new();
+        gScore.insert(start, 0);
+
+        let goal_distance_squared = |a: &TilePoint| {
+            let x = a.x as i32 - goal.x as i32;
+            let y = a.y as i32 - goal.y as i32;
+            (x * x + y * y) as u32
+        };
+        //let mut fScore = HashMap::new();
+        //fScore.insert(start, goal_distance_squared(&start));
+
+        let mut openSet = BinaryHeap::new();
+        openSet.push(PathState {
+            cost: goal_distance_squared(&start),
+            position: start,
+        });
+
+        while let Some(current) = openSet.pop() {
+            if current.position == goal {
+                return Some(reconstruct_path(cameFrom, &current.position));
+            }
+            closedSet.insert(current.position);
+
+            for neighbor in self.get_neighbors(&current.position) {
+                if !closedSet.contains(&neighbor) {
+                    let tentative_gScore = gScore.get(&current.position).unwrap().saturating_add(self.get_cost(&neighbor));
+                    let old_gScore = gScore.get(&neighbor).unwrap_or(&u32::MAX);
+                    if tentative_gScore < *old_gScore {
+                        openSet.push(PathState {
+                            cost: tentative_gScore + goal_distance_squared(&neighbor),
+                            position: neighbor,
+                        });
+                        cameFrom.insert(neighbor, current.position);
+                        gScore.insert(neighbor, tentative_gScore);
+                    }
+                }
+            }
+        }
+        
+        None
+    }
+    
+    fn get_neighbors(&self, point: &TilePoint) -> Vec<TilePoint> {
+        let mut neighbors = Vec::new();
+        if point.x > 0 {
+            neighbors.push(TilePoint::new(point.x - 1, point.y));
+        }
+        if point.x < self.width - 1 {
+            neighbors.push(TilePoint::new(point.x + 1, point.y));
+        }
+
+        if point.y > 0 {
+            neighbors.push(TilePoint::new(point.x, point.y - 1));
+        }
+        if point.y < self.height - 1 {
+            neighbors.push(TilePoint::new(point.x, point.y + 1));
+        }
+        neighbors
+    }
+
+    fn get_cost(&self, point: &TilePoint) -> u32 {
+        self.tile_costs[point.x][point.y]
+    }
+}
+
+fn reconstruct_path(cameFrom: HashMap<TilePoint, TilePoint>, goal: &TilePoint) -> Vec<TilePoint> {
+    let mut result = vec![*goal];
+    let mut current = goal;
+    while let Some(prev) = cameFrom.get(&current) {
+        current = prev;
+        result.push(*current);
+    }
+    result.into_iter().rev().collect()
+}
+
+impl TilePoint {
+    pub fn new(x: usize, y: usize) -> TilePoint {
+        TilePoint {
+            x: x,
+            y: y,
+        }
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ extern crate quicksilver;
 extern crate rand;
 extern crate rayon;
 
+mod geography;
+//mod human;
+
 use rand::distributions::{Distribution, Uniform, Normal};
 use rayon::prelude::*;
 
@@ -13,316 +16,69 @@ use quicksilver::{
     lifecycle::{Event, Settings, State, Window, run},
 };
 
-const WORLD_SIZE: usize = 100;
-const TICKS_PER_DAY: i32 = 10;
-const DAYS_PER_YEAR: i32 = 100;
-const TICKS_PER_YEAR: i32 = TICKS_PER_DAY * DAYS_PER_YEAR;
-const MAX_TEMP: i32 = 100;
-const MAX_CLOUD: i32 = 100;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::{self, BufReader};
 
+use std::u32;
+
+use geography::{Geography, TilePoint};
+
+/*
 struct GameState {
-    world: World,
-    view: ViewType,
-}
-
-enum ViewType {
-    Temp,
-    Cloud,
-}
-
-struct World {
-    counter: i32,
-
-    temps: WorldComponent<Vec<i32>>,
-    target_temps: WorldComponent<Vec<i32>>,
-
-    cloud_threshold: i32,
-    rain_threshold: i32,
-    clouds: WorldComponent<Vec<i32>>,
-    target_clouds: WorldComponent<Vec<i32>>,
-    // seasonal_cloud_phases: Vec<i32>,
-    // seasonal_cloud_amplitude: Vec<i32>,
-}
-
-struct WorldComponent<T> {
-    phase_t: T,
-    phase_f: T,
-    phase: bool,
-}
-
-impl<T> WorldComponent<T> {
-    fn phases(&mut self) -> (&T, &mut T) {
-        if self.phase {
-            (&self.phase_t, &mut self.phase_f)
-        } else {
-            (&self.phase_f, &mut self.phase_t)
-        }
-    }
-
-    fn swap(&mut self) {
-        self.phase = !self.phase
-    }
-}
-
-impl World {
-    fn tick(&mut self) {
-        {
-            let (temps, next_temps) = self.temps.phases();
-            let (target_temps, next_target_temps) = self.target_temps.phases();
-            let (clouds, next_clouds) = self.clouds.phases();
-            let (target_clouds, _next_target_clouds) = self.target_clouds.phases();
-
-            let cloud_threshold = &self.cloud_threshold;
-            let rain_threshold = &self.rain_threshold;
-
-            if self.counter % TICKS_PER_DAY == 0 {
-                // Update target temps
-                let equator = ((((TICKS_PER_YEAR / 2) - self.counter % TICKS_PER_YEAR).abs() - (TICKS_PER_YEAR / 4)) * (WORLD_SIZE as i32 / 3) / TICKS_PER_YEAR) + (WORLD_SIZE as i32 / 2);
-                next_target_temps.par_iter_mut().enumerate().for_each(|(n, next_target_temp)| {
-                    let y = n / WORLD_SIZE;
-                    let distance_from_equator = (equator - y as i32).abs();
-                    *next_target_temp = WORLD_SIZE as i32 - 2 * distance_from_equator;
-                });
-
-                // Update temps
-                let temp_sampler = Uniform::from(-10..10);
-                next_temps.par_iter_mut().enumerate().for_each(|(n, next_temp)| {
-                    let mut rng = rand::thread_rng();
-                    let i = n / WORLD_SIZE;
-                    let j = n % WORLD_SIZE;
-
-                    let mut divisor = 1;
-                    *next_temp = 0;
-                    *next_temp += temps[i * WORLD_SIZE + j];
-
-                    if i != (WORLD_SIZE - 1) {
-                        *next_temp += temps[(i + 1) * WORLD_SIZE + j];
-                        divisor += 1;
-                    }
-                    if i != 0 {
-                        *next_temp += temps[(i - 1) * WORLD_SIZE + j];
-                        divisor += 1;
-                    }
-
-                    if j != (WORLD_SIZE - 1) {
-                        *next_temp += temps[i * WORLD_SIZE + j + 1];
-                        divisor += 1;
-                        if i != (WORLD_SIZE - 1) {
-                            *next_temp += temps[(i + 1) * WORLD_SIZE + j + 1];
-                            divisor += 1;
-                        }
-                        if i != 0 {
-                            *next_temp += temps[(i - 1) * WORLD_SIZE + j + 1];
-                            divisor += 1;
-                        }
-                    }
-
-                    if j != 0 {
-                        *next_temp += temps[i * WORLD_SIZE + j - 1];
-                        divisor += 1;
-                        if i != (WORLD_SIZE - 1) {
-                            *next_temp += temps[(i + 1) * WORLD_SIZE + j - 1];
-                            divisor += 1;
-                        }
-                        if i != 0 {
-                            *next_temp += temps[(i - 1) * WORLD_SIZE + j - 1];
-                            divisor += 1;
-                        }
-                    }
-                    *next_temp += target_temps[i * WORLD_SIZE + j];
-                    *next_temp /= divisor + 1;
-                    *next_temp += temp_sampler.sample(&mut rng);
-
-                    if clouds[n] > *cloud_threshold {
-                        // up to 10 degrees cooler in the shade
-                        *next_temp -= (clouds[n].min(*rain_threshold) - *cloud_threshold) / 2;
-                    }
-                });
-                
-                // Update clouds
-                let cloud_sampler = Normal::new(0.0, 15.0);
-                next_clouds.par_iter_mut().enumerate().for_each(|(n, next_cloud)| {
-                    let mut rng = rand::thread_rng();
-                    let y = n / WORLD_SIZE;
-                    let x = n % WORLD_SIZE;
-
-                    let mut divisor = 1;
-                    *next_cloud = 0;
-                    *next_cloud += clouds[y * WORLD_SIZE + x];
-
-                    if y != (WORLD_SIZE - 1) {
-                        *next_cloud += clouds[(y + 1) * WORLD_SIZE + x];
-                        divisor += 1;
-                    }
-                    if y != 0 {
-                        *next_cloud += clouds[(y - 1) * WORLD_SIZE + x];
-                        divisor += 1;
-                    }
-
-                    if x != (WORLD_SIZE - 1) {
-                        *next_cloud += clouds[y * WORLD_SIZE + x + 1];
-                        divisor += 1;
-                        if y != (WORLD_SIZE - 1) {
-                            *next_cloud += clouds[(y + 1) * WORLD_SIZE + x + 1];
-                            divisor += 1;
-                        }
-                        if y != 0 {
-                            *next_cloud += clouds[(y - 1) * WORLD_SIZE + x + 1];
-                            divisor += 1;
-                        }
-                    }
-
-                    if x != 0 {
-                        *next_cloud += clouds[y * WORLD_SIZE + x - 1];
-                        divisor += 1;
-                        if y != (WORLD_SIZE - 1) {
-                            *next_cloud += clouds[(y + 1) * WORLD_SIZE + x - 1];
-                            divisor += 1;
-                        }
-                        if y != 0 {
-                            *next_cloud += clouds[(y - 1) * WORLD_SIZE + x - 1];
-                            divisor += 1;
-                        }
-                    }
-                    *next_cloud *= 5;
-                    divisor *= 5;
-
-                    *next_cloud += target_clouds[y * WORLD_SIZE + x];
-                    *next_cloud /= divisor + 1;
-                    *next_cloud += cloud_sampler.sample(&mut rng) as i32 * 2;
-                });
-            }
-        }
-
-        if self.counter % TICKS_PER_DAY == 0 {
-            self.temps.swap();
-            self.target_temps.swap();
-
-            self.clouds.swap();
-            self.target_clouds.swap();
-        }
-
-        self.counter += 1;
-    }
+    geography: geography::Geography,
+    humans: Vec<human::Human>,
 }
 
 impl State for GameState {
     fn new() -> Result<GameState> {
-        let mut temps = vec![0; WORLD_SIZE * WORLD_SIZE];
-        for x in 0..WORLD_SIZE {
-            for y in 0..WORLD_SIZE {
-                let distance_from_equator = ((WORLD_SIZE as i32 * 2 / 3) - y as i32).abs(); // start in winter
-                temps[y * WORLD_SIZE + x] = MAX_TEMP - 2 * distance_from_equator;
-            }
-        }
         Ok(GameState {
-            view: ViewType::Temp,
-            world: World {
-                counter: 0,
-
-                temps: WorldComponent {
-                    phase_t: temps.clone(),
-                    phase_f: temps.clone(),
-                    phase: true,
-                },
-                target_temps: WorldComponent {
-                    phase_t: temps.clone(),
-                    phase_f: temps,
-                    phase: true,
-                },
-
-                cloud_threshold: 50,
-                rain_threshold: 80,
-                clouds: WorldComponent {
-                    phase_t: vec![20; WORLD_SIZE * WORLD_SIZE],
-                    phase_f: vec![20; WORLD_SIZE * WORLD_SIZE],
-                    phase: true,
-                },
-                target_clouds: WorldComponent {
-                    phase_t: vec![20; WORLD_SIZE * WORLD_SIZE],
-                    phase_f: vec![20; WORLD_SIZE * WORLD_SIZE],
-                    phase: true,
-                },
-            },
         })
     }
 
     fn event(&mut self, event: &Event, window: &mut Window) -> Result<()> {
-            match *event {
-                Event::Key(Key::T, ButtonState::Pressed) => {
-                    self.view = ViewType::Temp;
-                }
-                Event::Key(Key::C, ButtonState::Pressed) => {
-                    self.view = ViewType::Cloud;
-                }
-                Event::Key(Key::Escape, ButtonState::Pressed) => {
-                    window.close();
-                }
-                _ => (),
-            }
             Ok(())
     }
 
     fn update(&mut self, _window: &mut Window) -> Result<()> {
-        self.world.tick();
         Ok(())
     }
 
     fn draw(&mut self, window: &mut Window) -> Result<()> {
-        match self.view {
-            ViewType::Temp => {
-                window.clear(Color::BLACK)?;
-
-                let (temps, _) = self.world.temps.phases();
-                for x in 0..WORLD_SIZE {
-                    for y in 0..WORLD_SIZE {
-                        window.draw(
-                            &Rectangle::new((x as i32 * 6 + WORLD_SIZE as i32, y as i32 * 6), (6, 6)), 
-                            Col(heatmap_value(temps[y * WORLD_SIZE + x], 0, MAX_TEMP, vec![Color::BLUE, Color::CYAN, Color::GREEN, Color::YELLOW, Color::RED])),
-                        );
-                    }
-                }
-
-                Ok(())
-            },
-            ViewType::Cloud => {
-                window.clear(Color::BLACK)?;
-
-                let (clouds, _) = self.world.clouds.phases();
-                for x in 0..WORLD_SIZE {
-                    for y in 0..WORLD_SIZE {
-                        window.draw(
-                            &Rectangle::new((x as i32 * 6 + WORLD_SIZE as i32, y as i32 * 6), (6, 6)), 
-                            Col(heatmap_value(clouds[y * WORLD_SIZE + x], 0, MAX_TEMP, vec![Color::BLACK, Color::BLACK, Color::WHITE, Color::BLUE])),
-                        );
-                    }
-                }
-
-                Ok(())
-            },
-        }
-    }
-}
-
-fn heatmap_value(value: i32, min: i32, max: i32, colors: Vec<Color>) -> Color {
-    if value >= max {
-        *colors.last().unwrap()
-    } else if value <= min {
-        *colors.first().unwrap()
-    } else {
-        let size = (max - min) as f32;
-        let norm = (value as f32 - min as f32) / size * (colors.len() - 1) as f32;
-        let fract = norm - norm.floor();
-        let idx1 = norm.floor() as usize;
-        let idx2 = norm.ceil() as usize;
-        let mut color = Color::WHITE;
-        color = color.with_red((colors[idx2].r - colors[idx1].r) * fract + colors[idx1].r);
-        color = color.with_green((colors[idx2].g - colors[idx1].g) * fract + colors[idx1].g);
-        color = color.with_blue((colors[idx2].b - colors[idx1].b) * fract + colors[idx1].b);
-        color
+        Ok(())
     }
 }
 
 fn main() {
     run::<GameState>("Esytheism", Vector::new(800, 600), Settings::default());
+}
+*/
+
+fn main() {
+    let file = File::open("data/test.map").unwrap();
+    let reader = BufReader::new(file);
+
+    let mut tile_costs: Vec<Vec<u32>> = reader.lines().filter_map(|line| line.ok()).map(|line| line.chars().map(|c| if c == '.' { 1 } else { u32::MAX }).collect()).collect();
+
+    // Transpose
+    for y in 0..40 {
+        for x in 0..y {
+            let temp = tile_costs[x][y];
+            tile_costs[x][y] = tile_costs[y][x];
+            tile_costs[y][x] = temp;
+        }
+    }
+
+    let geo = Geography::new(tile_costs.clone(), 40, 40);
+    let path = geo.find_path(
+        TilePoint::new(1, 39),
+        TilePoint::new(16, 35)
+    ).unwrap();
+
+    for y in 0..40 {
+        for x in 0..40 {
+            print!("{}", if path.iter().any(|t| *t == TilePoint::new(x, y)) { "x" } else if tile_costs[x][y] == 1 { "." } else { "+" })
+        }
+        println!();
+    }
 }
